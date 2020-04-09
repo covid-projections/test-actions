2f627bdac961f5bdfe12145463d045a39fec2bec
Ray Gilbert <>
2020-04-09 13:03:47 -0400

    Added create/update capability
    The script will now update existing routes when they exist and add new ones when they don't

--- symlinks.sh
+++ symlinks.sh
@@ -1,36 +1,24 @@
 #set -x
 
-
 # todo - add confirmation curls to each change to validate
 # todo need to fill in default rules if none are present?  Test this
-# todo ensure this is re-entrant for multiple mapping calls
 
 #
 # CONFIG - CHANGE AS NEEDED
 #
 AWS_CLI_BASE_CMD="aws --profile covid"
 BUCKET=raytest.rs.ring.com
-
-
-
-
-#DEFAULT_ROUTING_RULES="{\"IndexDocument\":{\"Suffix\":\"index.html\"},\"RoutingRules\":[{\"Condition\":{\"KeyPrefixEquals\":\"placeholdernotreal\"},\"Redirect\":{\"ReplaceKeyPrefixWith\":\"obsolete\"}}]}"
 OUTFILE="/tmp/routing%%.json"
 
-
-#
+######################################
 # This script tells S3 websites to redirect one path to another
 #
 # Sample usage
 #
-# ./symlinks.sh -s "v0/snap/foo" -d "v0/snap/latest"
-# curl http://raytest.rs.ring.com.s3-website-us-east-1.amazonaws.com/v0/snap/foo/ --verbose
-
-
-###
-# Main script
+# ./symlinks.sh -s "v0/snap/foo" -d "v0/snap/latest" [-practice]
 #
-#
+######################################
+
 # arg parsing from https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -60,17 +48,19 @@ esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
+
+
+
 echo "Setting routing redirects: mapping $PATH_TO_REDIRECT to $REDIRECT_DESTINATION"
 
-
+# Pull existing configuration
 current_config_file=/tmp/current-config$$.json
 $AWS_CLI_BASE_CMD s3api get-bucket-website --bucket $BUCKET >| $current_config_file
 current_config=$(<$current_config_file)
-new_config=""
-
 
 #echo $current_config | jq '.'
 i=0
+matched="no"
 for row in $(echo $current_config | jq '.RoutingRules[].Condition.KeyPrefixEquals');do
     #echo ${row} | jq -r '.[]'
     #echo $row
@@ -78,24 +68,31 @@ for row in $(echo $current_config | jq '.RoutingRules[].Condition.KeyPrefixEqual
         echo "Rule already exists, updating..."
         #todo - should check value and don't do anything if they already match
         new_config=$(echo $current_config | jq '.RoutingRules['$i'].Redirect.ReplaceKeyPrefixWith = '$REDIRECT_DESTINATION'')
-    else
-        echo "New rule!!"
+        matched="yes"
     fi
     i=$(expr $i + 1)
 done
 
+if [ "$matched" == "no" ]; then
+    echo "New rule"
+    new_config=$(echo $current_config | jq '.RoutingRules['$i'].Condition |= . + {"KeyPrefixEquals":'$PATH_TO_REDIRECT'}')
+    new_config=$(echo $new_config | jq '.RoutingRules['$i'].Redirect |= . + {"ReplaceKeyPrefixWith":'$REDIRECT_DESTINATION'}')
+fi
+
 echo $new_config >| $OUTFILE
 
-
+#
+# If practice mode, display results and exit w/out modifying S3
+#
 if [ "$PRACTICE" == "yes" ]; then
     cat $OUTFILE | jq -r
     echo "No changes applied to S3"
     exit 0
 fi
-#$AWS_CLI_BASE_CMD s3api put-bucket-website --bucket raytest.rs.ring.com --website-configuration file://$$.json
-#json_output="{\"IndexDocument\":{\"Suffix\":\"index.html\"},\"RoutingRules\":[{\"Condition\":{\"KeyPrefixEquals\":$PATH_TO_REDIRECT},\"Redirect\":{\"ReplaceKeyPrefixWith\":$REDIRECT_DESTINATION}}]}"
-#echo $json_output >| $$.json
 
+#
+# Write the new configuration
+#
 $AWS_CLI_BASE_CMD s3api put-bucket-website --bucket $BUCKET --website-configuration file://$OUTFILE
 
 if [ $? != 0 ]; then
